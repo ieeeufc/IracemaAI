664a08d51942ee1df286f69713c4dfd0035a8062
yanndrade <ybamello@hotmail.com>
2024-11-22 14:01:24 -0300

    fix: inserting to database

--- app.py
+++ app.py
@@ -3,26 +3,30 @@ import sys
 
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-from datetime import datetime
-
-import pandas as pd
 import streamlit as st
-from langchain_core.messages import AIMessage, HumanMessage
 from streamlit_feedback import streamlit_feedback
-from streamlit_gsheets import GSheetsConnection
+from langchain_core.messages import AIMessage, HumanMessage
 
 from src.config.assistant_config import IeeeAssistant
-from src.core.database import add_to_chroma
+from src.core.database import add_to_chroma, clear_database
 from src.core.loader import load_pdf_directory, split_documents
+from datetime import datetime
+from streamlit_gsheets import GSheetsConnection
 
-st.set_page_config(page_title='Iracema.AI', page_icon='💬', layout='centered')
+import uuid
+
+import pandas as pd
+
+# Configuração da página do Streamlit
+st.set_page_config(
+    page_title='Iracema.AI', page_icon='💬', layout='centered'
+)
 st.title('💬 Fale com a Iracema.IA')
 st.sidebar.title('Configurações')
 
 def _get_session():
     from streamlit.runtime import get_instance
     from streamlit.runtime.scriptrunner import get_script_run_ctx
-
     runtime = get_instance()
     session_id = get_script_run_ctx().session_id
     session_info = runtime._session_mgr.get_session_info(session_id)
@@ -30,14 +34,11 @@ def _get_session():
         raise RuntimeError("Couldn't get your Streamlit Session object.")
     return session_id
 
-
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 
-
 def current_time():
-    return datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-
+    return datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
 
 def reset_chat():
     st.session_state.pop('chat_history', None)
@@ -55,15 +56,13 @@ def get_response(user_input, chat_history=st.session_state.chat_history):
         st.session_state.assistant.get_assistant()
     return st.session_state.assistant.run_assistant(user_input, chat_history)
 
-
-# Configuração da página do Streamlit
-
 st.sidebar.button('Resetar Chat', on_click=reset_chat)
 
+conn = st.connection("gsheets", type=GSheetsConnection)
 
 for message in st.session_state.chat_history:
     if isinstance(message, AIMessage):
-        with st.chat_message('AI', avatar='assets/carcarA4.png'):
+        with st.chat_message('AI', avatar="assets/carcarA4.png"):
             st.write(message.content)
     elif isinstance(message, HumanMessage):
         with st.chat_message('Human'):
@@ -77,33 +76,29 @@ if user_query is not None and user_query != '':
     with st.chat_message('Human'):
         st.markdown(user_query)
 
-    with st.chat_message('AI', avatar='assets/carcarA4.png'):
+    with st.chat_message('AI',avatar="assets/carcarA4.png"):
         stream = get_response(user_query, st.session_state.chat_history)
         response = st.write_stream(stream)
     st.session_state.chat_history.append(AIMessage(content=response))
 
     streamlit_feedback(
-        feedback_type='thumbs',
-        optional_text_label='[Opcional] Por favor, for',
-        key='feedback',
-    )
+    feedback_type="thumbs",
+    optional_text_label="[Opcional] Por favor, forneça informações adicionais",
+    key="feedback")
 
-if 'feedback' in st.session_state and st.session_state['feedback'] is not None:
-    conn = st.connection("gsheets", type=GSheetsConnection)
+if "feedback" in st.session_state and st.session_state["feedback"] is not None:
     user_feedback = {
-        'session_id': _get_session(),
-        'inserted_at': current_time(),
-        'user_message': st.session_state['chat_history'][-2].content,
-        'assistant_message': st.session_state['chat_history'][-1].content,
-        'feedback_score': 1
-        if st.session_state['feedback']['score'] == '👍'
-        else 0,
-        'feedback_text': st.session_state['feedback']['text'],
-    }
+    "session_id": _get_session(),
+    "inserted_at": current_time(),
+    "user_message": st.session_state["chat_history"][-2].content,
+    "assistant_message": st.session_state["chat_history"][-1].content,
+    "feedback_score": 1 if st.session_state["feedback"]["score"] == "👍" else 0,
+    "feedback_text": st.session_state["feedback"]["text"]}
 
-    actual = conn.read(worksheet='feedback', max_entries=None)
-    update = pd.concat(
-        [actual, pd.DataFrame(user_feedback, index=[0])], ignore_index=True
-    )
-    conn.update(worksheet='feedback', data=update)
-    conn.reset()
+
+    update = pd.concat([conn.read(worksheet="feedback", max_entries=None), pd.DataFrame(user_feedback, index=[0])], ignore_index=True)
+    conn.update(worksheet="feedback", data=update,)
+
+st.cache_data.clear()
+conn.reset()
+    
